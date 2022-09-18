1c96d3d205e6c12e21de9ce64491047e17ce6894
Sehran <seh96.dev@gmail.com>
2022-09-19 00:59:36 +0800

    minor update

--- src/model/RegisterModel.js
+++ src/model/RegisterModel.js
@@ -1,7 +1,7 @@
 export default class RegisterModel {
     constructor (fullName,username, email, password, phone_number,state, city) {
         this.fullName = fullName === undefined ? '' : fullName;
-        this.username = username == undefined ? '' : username;
+        this.username = username === undefined ? '' : username;
         this.email = email === undefined ? '' : email;
         this.password = password === undefined ? '' : password;
         this.phone_number = phone_number === undefined ? '' : phone_number;
